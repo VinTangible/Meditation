6e414a19d3a792288bb4a3b899ee72183fd12748
Vincent Tang <vincenttang@vincents-macbook-pro.local>
2018-09-15 17:57:13 -0700

    Initial Commit

---
+++ Meditation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meditation
+//
+//  Created by Vincent Tang on 9/15/18.
+//  Copyright © 2018 Vincent Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
